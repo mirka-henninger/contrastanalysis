e14912fcedf7a2aae3f6be61745701dcb50f78b8
mirka-henninger <mail@mirka-henninger.de>
2019-10-24 18:20:26 +0200

    Update contrast_independent.R

--- R/contrast_independent.R
+++ R/contrast_independent.R
@@ -10,13 +10,36 @@ contrast_independent <- function(nGroup, lambda, dat){
   names(dat) <- c("groups", "values")
   # checks
   if(nGroup != length(unique(dat$groups)) & nGroup != ncol(lambda)) {
-    stop("Please check the data format: the first column must contain",
-         "the group indicator, the second the dependent variable.",
-         "nGroup must be the total number of between-subject groups.",
-         "lambda must contain the contrasts in the rows and the groups in columns")
+    stop("Please check the data format: the first column must contain ",
+         "the group indicator, the second the dependent variable. ",
+         "nGroup must be the total number of between-subject groups. ",
+         "lambda must contain the contrast weights in rows and the ",
+         "group indicator in columns")
   }
-  if(apply(lambda,1,rowSums) != 0){
-    stop("Your contrast weights do not sum to 0. Please check the weights!")
+  if(any(rowSums(lambda) != 0)){
+    stop("Your contrast weights do not sum to 0 for all contrasts. ",
+         "Please check the weights again!")
   }
 
+  # get group size, means and variances
+  groupVals <- dat %>% group_by(groups) %>% summarize(groupSize = length(values),
+                                                      groupMeans = mean(values),
+                                                      groupVariances = var(values))
+
+  numerator <- lambda %*% groupVals$groupMeans
+  denominator <- (lambda^2) %*% (groupVals$groupSize^(-1))
+  # dfContrast <- 1
+
+  # SS contrast
+  SScontrast <- (numerator^2) / denominator
+  SSwithin <- sum(groupVals$groupVariances)/nGroup
+  Fcontrast <- SScontrast / SSwithin
+  tcontrast <- numerator / sqrt(SSwithin * denominator)
+  output <- data.frame("SumsofSquares" = SScontrast,
+                       "F" = Fcontrast,
+                       "estimate" = numerator,
+                       "t" = tcontrast,
+                       "p" = pnorm(-abs(tcontrast))) %>% round(.,4)
+  row.names(output) <- paste0("Contrast ", row.names(output))
+  return(output)
 }
