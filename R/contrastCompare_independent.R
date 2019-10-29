753e05ed778c1d9b029906bb4ebf0f2203da0460
mirka-henninger <mail@mirka-henninger.de>
2019-10-29 15:56:52 +0100

    Update contrastCompare_independent.R

--- R/contrastCompare_independent.R
+++ R/contrastCompare_independent.R
@@ -7,7 +7,6 @@
 #' @param lambda1 a vector of contrast weights for Hypothesis 1
 #' @param lambda2 a vector of contrast weights for Hypothesis 2
 #' @param dat a matrix or dataframe with two columns; each row contains values for
-#' @param dat a matrix or dataframe with two columns; each row contains values for
 #' one respondents; the first column contains the group indicator, the second column
 #' contains the dependent variable
 #'
@@ -33,7 +32,8 @@
 #' @source Rosenthal et al. (2000); Sedlmeier & Renkewitz (2013)
 #'
 #' @examples
-#' # set.seed(1)
+#' library("dplyr")
+#' set.seed(1)
 #' nGroup <- 4
 #' lambda1 <- c(-1,-1,1,1)
 #' lambda2 <- c(-3,-1,1,3)
@@ -46,7 +46,7 @@
 #' @export
 contrastCompare_independent <- function(nGroup, lambda1, lambda2, dat){
 
-# checks on the input -----------------------------------------------------
+  # checks on the input -----------------------------------------------------
   names(dat) <- c("groups", "values")
   if(nGroup != length(unique(dat$groups))
      | nGroup != length(lambda1)
@@ -62,61 +62,23 @@ contrastCompare_independent <- function(nGroup, lambda1, lambda2, dat){
   }
 
 
-# standardize lambda weight -----------------------------------------------
+  # standardize lambda weight -----------------------------------------------
   lambda1Std <- lambda1 / sqrt(mean(lambda1^2))
   lambda2Std <- lambda2 / sqrt(mean(lambda2^2))
-  lambdaDiff <- lambda1Std - lambda2Std
+  lambdaDiff <- lambda1Std - lambda2Std %>% as.matrix() %>% t()
 
+  results <- contrast_independent(nGroup = nGroup,
+                                  lambda = lambdaDiff,
+                                  dat = dat)
 
-# get group size, means and variances -------------------------------------
-  groupVals <- dat %>% group_by(groups) %>% summarize(groupSize = length(values),
-                                                      groupMeans = mean(values),
-                                                      groupVariances = var(values))
+  weights <- data.frame(
+    lambda1Std = lambda1Std,
+    lambda2Std = lambda2Std,
+    lambdaDiff = lambdaDiff %>% as.vector()
+  )
 
-
-# define contrast estimate ------------------------------------------------
-  numerator <- lambdaDiff %*% groupVals$groupMeans
-  denominator <- (lambdaDiff^2) %*% (groupVals$groupSize^(-1))
-  # dfContrast <- 1
-
-
-# SS contrast and SS within -----------------------------------------------
-  SScontrast <- (numerator^2) / denominator
-  MSwithin <- sum(groupVals$groupVariances)/nGroup
-  # SSwithin <- (nrow(dat) - nGroup) * MSwithin
-
-
-# F and t values ----------------------------------------------------------
-  Fcontrast <- SScontrast / MSwithin
-  tcontrast <- numerator / sqrt(MSwithin * denominator)
-  pval <- 2*pnorm(-abs(tcontrast))
-
-
-# effect sizes ------------------------------------------------------------
-  # r effectsize
-  groupNames <- unique(dat$groups)
-  names(lambdaDiff) <- groupNames
-  dat <- cbind(dat,lambdaDiff = lambdaDiff[match(dat$groups,names(lambdaDiff))])
-  r_effectsize <- cor(dat[,-1])[1,-1]
-  # r alerting
-  r_alerting <- c(cor(groupVals$groupMeans,lambdaDiff))
-  # r contrast
-  r_contrast <- sqrt(tcontrast^2/(tcontrast^2+nrow(dat)-nGroup))
-
-
-# format output -----------------------------------------------------------
-  rounding <- 4
-  output <- data.frame("SumsofSquares" = SScontrast %>% round(.,rounding),
-                       "F" = Fcontrast %>% round(.,rounding),
-                       "estimate" = numerator %>% round(.,rounding),
-                       "t" = tcontrast %>% round(.,rounding),
-                       "p" = pval %>% round(.,rounding),
-                       "rEffectSize" = r_effectsize %>% round(.,rounding),
-                       "rAlerting" = r_alerting %>% round(.,rounding),
-                       "r2Alerting" = r_alerting^2 %>% round(.,rounding),
-                       "rContrast" = r_contrast %>% round(.,rounding))
-  row.names(output) <- paste0("Contrast ", row.names(output))
-
+  output <- list(results = results,
+                 contrastWeights = weights)
   return(output)
 }
 
