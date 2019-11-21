618a49779616888b24ea9765bf13c531a1ca0a3d
mirka-henninger <mail@mirka-henninger.de>
2019-11-21 18:03:05 +0100

    add contrastCompare_dependent

---
+++ R/contrastCompare_dependent.R
@@ -0,0 +1,81 @@
+#' Contrast analyses for dependent samples
+#'
+#' This function allows to directly compare two contrasts for dependent samples by
+#' standardizing the contrast weights
+#'
+#' @param nGroup Number of dependent / within-subject groups
+#' @param lambda1 a vector of contrast weights for Hypothesis 1
+#' @param lambda2 a vector of contrast weights for Hypothesis 2
+#' @param dat a matrix or dataframe with nGroup columns; each row contains values for
+#' one respondent;
+#' each column contains values of the dependent variable in the respective within-
+#' subject group
+#' @param testvalue value under the Null hypothesis; if not specified, it is fixed to 0.
+#'
+#' @return a dataframe with following entries for each of the contrasts:
+#' \describe{
+#'   \item{\code{SumsofSquares}}{Sums of Squares}
+#'   \item{\code{F}}{F-values}
+#'   \item{\code{estimate}}{Contrast estimates}
+#'   \item{\code{t}}{t-values}
+#'   \item{\code{p}}{two-tailed p-values}
+#'   \item{\code{g}}{effect size g, a standardized distance measure}
+#'
+#' @note A test favoring Hypothesis 1 is performed, hence a positive t-value indicate that the
+#' contrast weights contained in lambda1 fit the data better than the contrast weights
+#' contained in lambda2, and vice versa for a negative t-value.
+#'
+#' @source Rosenthal et al. (2000); Sedlmeier & Renkewitz (2013)
+#'
+#' @examples
+#' nGroup <- 4
+#' N <- 50
+#' lambda1 <- c(3,-1,-1,-1)
+#' lambda2 <- c(0,2,-1,-1)
+#' dat <- data.frame(
+#'   group1 = sample(1:10, size = N, replace = TRUE),
+#'   group2 = sample(1:15, size = N, replace = TRUE),
+#'   group3 = sample(1:15, size = N, replace = TRUE),
+#'   group4 = sample(1:20, size = N, replace = TRUE)
+#' )
+#' contrastCompare_dependent(nGroup, lambda1, lambda2, dat)
+#'
+#' @export
+contrastCompare_dependent <- function(nGroup, lambda1, lambda2, dat, testvalue = 0){
+
+  # checks on the input -----------------------------------------------------
+  names(dat) <- paste0("group", 1:nGroup)
+  if(nGroup != ncol(dat)
+    | nGroup != length(lambda1)
+    | nGroup != length(lambda2)) {
+    stop("Please check the data format: each column must contain ",
+         "the dependent variable in the within-subject group.",
+         "nGroup must be the total number of within-subject groups. ",
+         "lambda1 and lambda2 must each contain one set of contrast weights")
+  }
+if(sum(lambda1) != 0 | sum(lambda2) != 0){
+  stop("Your contrast weights do not sum to 0 for all contrasts. ",
+       "Please check the weights again!")
+}
+
+
+# standardize lambda weight -----------------------------------------------
+lambda1Std <- lambda1 / sqrt(mean(lambda1^2))
+lambda2Std <- lambda2 / sqrt(mean(lambda2^2))
+lambdaDiff <- lambda1Std - lambda2Std %>% as.matrix() %>% t()
+
+results <- contrast_dependent(nGroup = nGroup,
+                              lambda = lambdaDiff,
+                              dat = dat)
+
+weights <- data.frame(
+  lambda1Std = lambda1Std,
+  lambda2Std = lambda2Std,
+  lambdaDiff = lambdaDiff %>% as.vector()
+)
+
+output <- list(results = results,
+               contrastWeights = weights)
+return(output)
+}
+
