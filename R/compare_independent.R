628c0a80feb67169d1ceff56551b0fdd2fa83b00
Mirka Henninger <mail@mirka-henninger.de>
2022-11-11 15:29:21 +0100

    improve readability of preferred contrast

--- R/compare_independent.R
+++ R/compare_independent.R
@@ -4,8 +4,8 @@
 #' standardizing the contrast weights
 #'
 #' @param n_group Number of independent / between-subject groups
-#' @param lambda1 A vector of contrast weights for Hypothesis 1
-#' @param lambda2 A vector of contrast weights for Hypothesis 2
+#' @param lambda_preferred A vector of contrast weights for Hypothesis 1
+#' @param lambda_competing A vector of contrast weights for Hypothesis 2
 #' @param data A matrix or dataframe with two columns; each row contains values for
 #'  one respondents; the first column contains the group indicator, the second column
 #'  contains the dependent variable
@@ -17,9 +17,10 @@
 #'    contrasts and difference between standardized contrast weights}
 #' }
 #'
-#' @note A test favoring Hypothesis 1 is performed, hence a positive t-value indicate
-#'  that the contrast weights contained in lambda1 fit the data better than the contrast
-#'  weights contained in lambda2, and vice versa for a negative t-value.
+#' @note A test favoring Hypothesis 1 is performed. This means that when the t-value is positive,
+#'  the contrast weights contained in lambda_preferred reflect the data better than the contrast weights
+#'  contained in lambda_competing. When the t-value is negative, the contrast weights contained in
+#'  lambda_competing reflect the data better, and Hypothesis 2 is favored over Hypothesis 1.
 #'
 #' @source Rosenthal et al. (2000); Sedlmeier & Renkewitz (2013)
 #'
@@ -29,48 +30,48 @@
 #' iris <- iris[,c("Species", "Petal.Length")]
 #'
 #' # define lambda weights
-#' lambda1 <- c(-1, 0, 1)  # H1: Iris versicolor is distinct to Iris setosa and Iris virginica
-#' lambda2 <- c(-2, 1, 1)  # H2: Iris versicolor is more similar to Iris virginica
+#' lambda_preferred <- c(-1, 0, 1)  # H1: Iris versicolor is distinct to Iris setosa and Iris virginica
+#' lambda_competing <- c(-2, 1, 1)  # H2: Iris versicolor is more similar to Iris virginica
 #'
 #' # perform contrast analysis
-#' compare_independent(n_group=3, lambda1, lambda2, iris)
+#' compare_independent(n_group=3, lambda_preferred, lambda_competing, iris)
 #'
 #' @export
 compare_independent <- function(n_group,
-                                lambda1,
-                                lambda2,
+                                lambda_preferred,
+                                lambda_competing,
                                 data) {
 
 
   # Checks on the input -----------------------------------------------------
   names(data) <- c("groups", "values")
   if (n_group != length(unique(data$groups))
-      | n_group != length(lambda1)
-      | n_group != length(lambda2)) {
+      | n_group != length(lambda_preferred)
+      | n_group != length(lambda_competing)) {
     stop("Please check the data format: \n",
          " * the first column must contain the group indicator \n",
          " * the second the dependent variable \n",
          " * n_group must be the total number of between-subject groups \n",
-         " * lambda1 and lambda2 must each contain one set of contrast weights")
+         " * lambda_preferred and lambda_competing must each contain one set of contrast weights")
   }
-  if (sum(lambda1) != 0 | sum(lambda2) != 0) {
+  if (sum(lambda_preferred) != 0 | sum(lambda_competing) != 0) {
     stop("Your contrast weights do not sum to 0 for all contrasts. ",
          "Please check the weights again!")
   }
 
 
   # Standardize lambda weight -----------------------------------------------
-  lambda1_std <- lambda1 / sqrt(mean(lambda1^2))
-  lambda2_std <- lambda2 / sqrt(mean(lambda2^2))
-  lambda_diff <- t(as.matrix(lambda1_std - lambda2_std))
+  lambda_preferred_std <- lambda_preferred / sqrt(mean(lambda_preferred^2))
+  lambda_competing_std <- lambda_competing / sqrt(mean(lambda_competing^2))
+  lambda_diff <- t(as.matrix(lambda_preferred_std - lambda_competing_std))
 
   results <- contrast_independent(n_group = n_group,
                                   lambda = lambda_diff,
                                   data = data)
 
   weights <- data.frame(
-    lambda1_std = lambda1_std,
-    lambda2_std = lambda2_std,
+    lambda_preferred_std = lambda_preferred_std,
+    lambda_competing_std = lambda_competing_std,
     lambda_diff = as.vector(lambda_diff)
   )
 
